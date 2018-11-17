a19f568e444de90936cfb54c9c2a5efd5a8b49f1
Milan Vit <milanvit@milanvit.net>
2018-11-17 19:32:02 +0900

    Implemented interfaces for services
    Closes #5

---
+++ pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/service/VetService.java
@@ -0,0 +1,13 @@
+package net.milanvit.sfgpetclinic.service;
+
+import net.milanvit.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
