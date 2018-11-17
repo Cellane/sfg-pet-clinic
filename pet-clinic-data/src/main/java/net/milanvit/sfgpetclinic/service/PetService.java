aa8f98e4cd28e5b5b7cb4058927174dc70ae5950
Milan Vit <milanvit@milanvit.net>
2018-11-17 23:25:50 +0900

    Refactored to common CRUD service
    Closes #11

--- pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/service/PetService.java
@@ -2,12 +2,5 @@ package net.milanvit.sfgpetclinic.service;
 
 import net.milanvit.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
