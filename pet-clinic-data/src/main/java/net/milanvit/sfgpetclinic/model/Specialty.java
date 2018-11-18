5f4fefc972537699e6ed61bad956a28166ad98b8
Milan Vit <milanvit@milanvit.net>
2018-11-19 00:53:32 +0900

    Updated Vet entity for JPA
    Closes #37

--- pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/model/Specialty.java
@@ -4,8 +4,15 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.milanvit.sfgpetclinic.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 }
