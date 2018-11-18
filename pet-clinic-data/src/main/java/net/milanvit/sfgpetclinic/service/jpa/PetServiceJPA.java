bba861606e4cbcfa3ac44cf60f2935e6cb366e13
Milan Vit <milanvit@milanvit.net>
2018-11-19 01:36:13 +0900

    Implemented Spring Data JPA service for Pets
    Closes #43

---
+++ pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/service/jpa/PetServiceJPA.java
@@ -0,0 +1,49 @@
+package net.milanvit.sfgpetclinic.service.jpa;
+
+import net.milanvit.sfgpetclinic.model.Pet;
+import net.milanvit.sfgpetclinic.repository.PetRepository;
+import net.milanvit.sfgpetclinic.service.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetServiceJPA implements PetService {
+    private final PetRepository petRepository;
+
+    public PetServiceJPA(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+
+        petRepository.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
