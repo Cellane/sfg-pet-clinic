6cb1dbd5d5be3b3037213cc9cfd6aa106e4e40a5
Milan Vit <milanvit@milanvit.net>
2018-11-18 19:44:49 +0900

    Added Pet Type to bootstrap
    Closes #32

--- pet-clinic-web/src/main/java/net/milanvit/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/net/milanvit/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package net.milanvit.sfgpetclinic.bootstrap;
 
 import net.milanvit.sfgpetclinic.model.Owner;
+import net.milanvit.sfgpetclinic.model.PetType;
 import net.milanvit.sfgpetclinic.model.Vet;
 import net.milanvit.sfgpetclinic.service.OwnerService;
+import net.milanvit.sfgpetclinic.service.PetTypeService;
 import net.milanvit.sfgpetclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,27 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        // PET TYPE
+        PetType dog = new PetType();
+        PetType cat = new PetType();
+
+        dog.setName("Dog");
+        cat.setName("Cat");
+
+        PetType savedDog = petTypeService.save(dog);
+        PetType savedCat = petTypeService.save(cat);
+
+        // OWNER
         Owner owner1 = new Owner();
         Owner owner2 = new Owner();
 
@@ -30,6 +45,7 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
         ownerService.save(owner2);
 
+        // VET
         Vet vet1 = new Vet();
         Vet vet2 = new Vet();
 
