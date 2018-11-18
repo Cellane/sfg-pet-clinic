01783216d5311a540f5034226edfcdd47d1b837a
Milan Vit <milanvit@milanvit.net>
2018-11-18 20:12:34 +0900

    Added Specialty map implementation
    Closes #31

---
+++ pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/service/SpecialtyService.java
@@ -0,0 +1,6 @@
+package net.milanvit.sfgpetclinic.service;
+
+import net.milanvit.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/service/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package net.milanvit.sfgpetclinic.service.map;
+
+import net.milanvit.sfgpetclinic.model.Specialty;
+import net.milanvit.sfgpetclinic.service.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
