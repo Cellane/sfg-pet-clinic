9564091517561b24d3c56f2a762975c6933ffe10
Milan Vit <milanvit@milanvit.net>
2018-11-19 01:27:22 +0900

    Implemented Spring Data JPA service for Owners
    Closes #40

---
+++ pet-clinic-data/src/main/java/net/milanvit/sfgpetclinic/service/jpa/OwnerServiceJPA.java
@@ -0,0 +1,61 @@
+package net.milanvit.sfgpetclinic.service.jpa;
+
+import net.milanvit.sfgpetclinic.model.Owner;
+import net.milanvit.sfgpetclinic.repository.OwnerRepository;
+import net.milanvit.sfgpetclinic.repository.PetRepository;
+import net.milanvit.sfgpetclinic.repository.PetTypeRepository;
+import net.milanvit.sfgpetclinic.service.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class OwnerServiceJPA implements OwnerService {
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerServiceJPA(OwnerRepository ownerRepository, PetRepository petRepository,
+                           PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+
+        ownerRepository.findAll().forEach(owners::add);
+
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
