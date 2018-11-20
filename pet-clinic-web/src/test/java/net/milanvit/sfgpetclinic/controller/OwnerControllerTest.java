129ba6de0e1727e39251f37a7b3999915bcde058
Milan Vit <milanvit@milanvit.net>
2018-11-20 23:48:20 +0900

    Added owner detail screen
    Closes #53

--- pet-clinic-web/src/test/java/net/milanvit/sfgpetclinic/controller/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/net/milanvit/sfgpetclinic/controller/OwnerControllerTest.java
@@ -15,21 +15,22 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
-    @Mock
-    private OwnerService ownerService;
-
     @InjectMocks
     OwnerController controller;
 
     MockMvc mockMvc;
     Set<Owner> owners;
+
+    @Mock
+    private OwnerService ownerService;
 
     @BeforeEach
     void setUp() {
@@ -56,5 +57,15 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners/find"))
             .andExpect(status().isOk())
             .andExpect(view().name("owners/find"));
+    }
+
+    @Test
+    void showOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/1"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("owners/ownerDetails"))
+            .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
