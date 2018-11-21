c4b410816ddc6d697329a1d7159a1b92dfeba588
Milan Vit <milanvit@milanvit.net>
2018-11-21 09:06:51 +0900

    Added model attributes for Pet controller.
    Related to #56

---
+++ pet-clinic-web/src/main/java/net/milanvit/sfgpetclinic/controller/PetController.java
@@ -0,0 +1,44 @@
+package net.milanvit.sfgpetclinic.controller;
+
+import net.milanvit.sfgpetclinic.model.Owner;
+import net.milanvit.sfgpetclinic.model.PetType;
+import net.milanvit.sfgpetclinic.service.OwnerService;
+import net.milanvit.sfgpetclinic.service.PetService;
+import net.milanvit.sfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
+
+    public PetController(PetService petService, PetTypeService petTypeService, OwnerService ownerService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
+    }
+
+    @ModelAttribute("petTypes")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
