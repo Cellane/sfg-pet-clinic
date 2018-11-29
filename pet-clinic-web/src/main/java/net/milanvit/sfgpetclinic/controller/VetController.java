f9b819c7da771bd2d1cafb12c3e99fe0b4521f77
Milan Vit <milanvit@milanvit.net>
2018-11-29 19:54:32 +0900

    Added Vets JSON endpoint
    Closes #62

--- pet-clinic-web/src/main/java/net/milanvit/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/net/milanvit/sfgpetclinic/controller/VetController.java
@@ -1,12 +1,14 @@
 package net.milanvit.sfgpetclinic.controller;
 
+import net.milanvit.sfgpetclinic.model.Vet;
 import net.milanvit.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/vets")
+import java.util.List;
+
 @Controller
 public class VetController {
     private final VetService vetService;
@@ -15,10 +17,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @GetMapping("")
+    @GetMapping("/vets")
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody List<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
